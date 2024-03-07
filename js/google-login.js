14c72c9158fd0ad9210a3d12967463c6db6df824
AzmanAthar <160365638+AzmanAthar@users.noreply.github.com>
2024-03-07 20:48:06 +0000

    Update google-login.js

--- js/google-login.js
+++ js/google-login.js
@@ -2,13 +2,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBEHLroiF9f5bFCUYoSvyTASCE1IRLv_Ig",
-    authDomain: "upheld-fold-393315.firebaseapp.com",
-    projectId: "upheld-fold-393315",
-    storageBucket: "upheld-fold-393315.appspot.com",
-    messagingSenderId: "182938650769",
-    appId: "1:182938650769:web:2c8de9e7e2c57a03679730",
-    measurementId: "G-6JEGBBKGTV"
+    
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
